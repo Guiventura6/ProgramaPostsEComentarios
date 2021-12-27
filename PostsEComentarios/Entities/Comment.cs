3a72984f3499651d5d59441d6e00c9cb6fffc44d
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 17:41:16 -0300

    Criacao da classe Comment

---
+++ PostsEComentarios/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostsEComentarios.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
