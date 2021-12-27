06994ddbb616465485c6bd04bf3322180114f327
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 18:07:02 -0300

    Instanciacao manual dos objetos

--- PostsEComentarios/Program.cs
+++ PostsEComentarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PostsEComentarios.Entities;
 using System.Collections.Generic;
 
 namespace PostsEComentarios
@@ -7,7 +8,25 @@ namespace PostsEComentarios
     {
         static void Main(string[] args)
         {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
             
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                12);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
 
         }
     }
